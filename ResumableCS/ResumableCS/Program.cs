93f28189a914a56eec05506414c2360ec35bbf1f
senghuot <senghuotlim@gmail.com>
2015-09-14 17:20:22 -0700

    adding resumable feature

--- ResumableCS/ResumableCS/Program.cs
+++ ResumableCS/ResumableCS/Program.cs
@@ -8,25 +8,25 @@ using System.IO;
 namespace ResumableCS {
     class Program {
         static void Main(string[] args) {
-            // instantiate resumbale object 
-            var chunksize = 10 * 1024 * 1024;
-            var target = "http://localhost:3000/api/upload";
-            Resumable resumable = new Resumable(chunksize, target);
+            // instantiate resumbale object
+            var local = "mongodb://localhost:27017/";
+            var skydrive = "mongodb://skyd-vap-mdbp1.devop.vertafore.com:27017/";
+            var database = "test";
+            var chunksize = 1024 * 1024; // in bytes
+            var simultaneousUploads = 10;
+            Resumable resumable = new Resumable(chunksize, skydrive, database, simultaneousUploads);
 
-            // infinte loop till user type "quit" 
-            var exitKeyword = "quit";
+            // infinte loop
+            var root = @"C:\Users\limse\Downloads\";
             while (true) {
                 // grab the input from user
                 Console.Write("Enter a filename to upload: ");
                 var input = Console.ReadLine();
-                input = @"C:\Users\limse\Downloads\oversize.pdf";
-                // exit if the input matches the exitKeyword
-                if (input == exitKeyword)
-                    return;
-                
+                input = root + input;
+
                 // not currently support the directory, maybe we only have to extend this
                 // functionality with recursion in resumble.
-                else if (Directory.Exists(input)) {
+                if (Directory.Exists(input)) {
                     Console.WriteLine("We don't support directory yet");
 
                 // handles a single file
--- ResumableCS/ResumableCS/Resumable.cs
+++ ResumableCS/ResumableCS/Resumable.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.IO;
 using MongoDB.Driver;
@@ -19,60 +20,126 @@ namespace ResumableCS {
      * need to calll the right method and let the library handles all the details work.
      */
     class Resumable {
+        // these static variable are used to test uploading speed and file integrity
+        private static bool UNIT_TEST = true;
+        private static int TEST_LOOP = 1;
+        private static int DELAY = 250; // how long the task/thread will sleep if connection lost
+        // mongodb only allow 16MB per grid.fs.file upload request
+        private static int MAX_CHUNK_SIZE = 16 * 1024 * 1024;
+
         // these parameter might get changed as the developement
         private int chunksize;
         private string target;
+        private int simultaneousUploads;
+        private string database;
 
         // default constructor
-        public Resumable() {
-            this.chunksize = 1024 * 1024;
-            this.target = "http://localhost:3000/api/upload";
+        public Resumable()
+            : this(1024 * 1024, "mongodb://localhost:27017/", "test", 10) {
+            // call modifiable constructor instead
         }
 
         // modifiable contructor
-        public Resumable(int chunksize, string target) {
+        public Resumable(int chunksize, string target, string database, int simultaneousUploads) {
+            if (chunksize > MAX_CHUNK_SIZE)
+                throw new ArgumentException("Chunksize can not exceed 16MB.");
+            this.database = database;
             this.chunksize = chunksize;
             this.target = target;
+            this.simultaneousUploads = simultaneousUploads;
         }
 
         // setup connections before any uploading could take place
         public void upload(string filename) {
-            var server = new MongoClient("mongodb://localhost:27017/").GetServer();
-            var grid = server.GetDatabase("test").GridFS;
-
-            // break the file into many small chunks
-            var chunks      = getChunks(filename);
-            var metadata    = getMetadata(filename);
-            //var metadata = new BsonDocument {{"filename", filename}, {"length", lengh}, {"chunkSize", 261120},
-            //                                 {"uploadDate", DateTime.UtcNow}, {"md5", "8eb1ce6048d14ce17dd759849c2dc0b4"}};
-            
-            grid.Files.Insert(metadata);
-            var id = metadata.GetElement("_id").ToString().Substring(4);
-
-            var tasks = new Task[chunks.Count];
             Stopwatch timer = new Stopwatch();
             timer.Start();
+            List<byte[]> chunks     = null;
+            MongoGridFS grid        = null;
+            BsonDocument metadata   = null;
+            string id               = null;
+            for (var i = 0; i < TEST_LOOP; i++) {
+                var server = new MongoClient(target).GetServer();
+                grid = server.GetDatabase(database).GridFS;
+
+                // break the file into many small chunks
+                chunks          = getChunks(filename);
+                metadata        = getMetadata(filename);
+
+                // it is important to first write a simple header decoration then grab the id
+                // to upload the chunks.
+                grid.Files.Insert(metadata);
+                id = metadata.GetElement("_id").ToString().Substring(4);
+                multiThreadedUpload(chunks, id, grid);
+            }
+            timer.Stop();
+            report(timer, filename, chunks);
+
+            // check upload file integrity
+            if (UNIT_TEST)
+                checkFileIntegrity(grid, id, metadata);
+        }
+
+        // check if the uploaded file integrity
+        private void checkFileIntegrity(MongoGridFS grid, string id, BsonDocument metadata) {
+            Console.Write("Checking file upload integrity: ");
+            var downloadInfo = grid.FindOneById(ObjectId.Parse(id));
+            var downloadMD5 = getMD5(downloadInfo.OpenRead());
+            if (metadata["md5"] == downloadMD5)
+                Console.WriteLine("PASS");
+            else
+                Console.WriteLine("FAIL");
+        }
+
+        // display report data
+        private void report(Stopwatch timer, string filename ,List<Byte[]> chunks) {
+            using (var stream = new FileStream(filename, FileMode.Open)) {
+                Console.WriteLine("Filesize: " + bytesToSize(stream.Length));
+                Console.WriteLine("simultaneous Uploads: " + simultaneousUploads);
+                Console.WriteLine("Avg Time: " + timer.ElapsedMilliseconds / TEST_LOOP + "ms");
+            }
+        }
+
+        // converting storage units. mainly use to report data 
+        private string bytesToSize(double size) {
+            var sizes = new string[] {"Bytes", "KB", "MB", "GB", "TB"};
+            if (size == 0) return "0 " + sizes[0];
+            var i = Convert.ToInt32(Math.Floor(Math.Log(size) / Math.Log(1024)));
+            return Math.Round(size / Math.Pow(1024, i), 2) + " " + sizes[i];
+        }
+
+        // uploading all the chunks into grid.fs.files asynchronously but we have to watch out for
+        // resource competition. so we will only allow upto 'simultaneousUploads' tasks run at a time.
+        // this ensure that gridfs to not overwhelm with write requests.
+        public void multiThreadedUpload(List<byte[]> chunks, string id, MongoGridFS grid) {
+            SemaphoreSlim max = new SemaphoreSlim(simultaneousUploads);
+            var tasks = new Task[chunks.Count];
             for (var i = 0; i < chunks.Count; i++) {
+                max.Wait();
                 var index = i;
-                var chunkMetadata = new BsonDocument { { "files_id", id }, { "n", i }, { "data", chunks[i] } };
-                tasks[i] = Task.Factory.StartNew(() => writeToGrid(chunkMetadata, grid, index));
+                var chunkMetadata = new BsonDocument { { "files_id", new ObjectId(id) }, { "n", i }, { "data", chunks[i] } };
+                tasks[i] = Task.Factory.StartNew(() => writeToGrid(chunkMetadata, grid, index)
+                    , TaskCreationOptions.LongRunning)
+                    .ContinueWith( (task) => max.Release());
             }
             Task.WaitAll(tasks);
-            timer.Stop();
-            Console.WriteLine("DONE: " + timer.ElapsedMilliseconds.ToString());
         }
 
         // this method will write stuff to the database
         private void writeToGrid(BsonDocument chunkMetadata, MongoGridFS grid, int index) {
             while (true) {
-                var result = grid.Chunks.Insert(chunkMetadata);
-                var response = result.Response.GetElement("ok").Value.AsInt32;
-                if (response == 1) {
-                    // do something
-                    Console.WriteLine("Chunk " + index + " uploaded complete");
-                    break;
-                } else {
-                    Task.Delay(250);
+                try {
+                    var result = grid.Chunks.Insert(chunkMetadata);
+                    var response = result.Response.GetElement("ok").Value.AsInt32;
+                    if (response == 1) {
+                        Console.WriteLine("Chunk " + index + " uploaded.");
+                        break;
+                    } else {
+                        Console.WriteLine("Chunk " + index + " corrupt upload...resume in " + DELAY + "ms");
+                        Task.Delay(DELAY);
+                    }
+                } catch (MongoConnectionException) {
+                    Console.WriteLine("Chunk " + index + " lost connection...resume in " + DELAY + "ms");
+                    Task.Delay(DELAY);
                 }
             }
         }
@@ -106,7 +173,7 @@ namespace ResumableCS {
         }
 
         // computing md5
-        private string getMD5(FileStream stream) {
+        private string getMD5(Stream stream) {
             using (var md5 = MD5.Create()) {
                 byte[] data = md5.ComputeHash(stream);
                 var res = new StringBuilder();
